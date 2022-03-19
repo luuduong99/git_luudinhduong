0ba2d7b4385eb985778fe6e1f67d7448338116c8
LUU DUONG <luudinhduong2806@gmail.com>
2022-03-19 09:48:38 +0700

    PHP oop

---
+++ PracticeOop2.php
@@ -0,0 +1,73 @@
+<?php
+
+    abstract class Country
+    {
+	    use Active;
+        protected $slogan;
+
+        abstract public function sayHello();
+
+        public function setSlogan ($slogan) 
+        {
+            $this->slogan = $slogan;
+        }
+    }
+
+    interface Boss 
+    {
+        public function checkValidSlogan();
+    }
+
+    class EnglandCountry extends Country implements Boss
+    {
+        public function sayHello() 
+        {
+            $england = "england";
+            $english = "english";
+
+            return strpos($this->slogan, $england) !== false || strpos($this->slogan, $english) !== false;
+
+        }
+        public function checkValidSlogan() 
+        {
+            if($this->sayHello() !== false) {
+                return true;
+            } return false;
+        }
+
+    }
+
+
+    class VietnamCountry extends Country implements Boss 
+    {
+        public function sayHello() 
+        {
+            $vietnam = "vietnam";
+            $hust = "hust";
+
+            return strpos($this->slogan, $vietnam) !== false && strpos($this->slogan, $hust) !== false;
+        }
+
+        public function checkValidSlogan() 
+        {
+            if($this->sayHello() !== false) {
+                return true;
+            } return false;
+    
+        }
+
+    }
+    
+    Trait Active 
+    {
+ 	    public function defindYourSelf()
+        {
+ 		    return get_class($this);
+ 	    }
+    }
+    $EnglandCountry = new EnglandCountry();
+    $VietnamCountry = new VietnamCountry();
+
+    echo 'I am ' . $EnglandCountry->defindYourSelf(); 
+    echo "<br>";
+    echo 'I am ' . $VietnamCountry->defindYourSelf(); 
