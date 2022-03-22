f79ce805525788500fd0774b7b3395b5ebb8fac6
LUU DUONG <luudinhduong2806@gmail.com>
2022-03-22 11:38:53 +0700

    Fix php basic

--- exercise1.php
+++ exercise1.php
@@ -1,19 +1,21 @@
 <?php
     
-    function checkValidString($temp)
+    function checkValidString($file)
     {
         $book = "book";
         $res = "restaurant";
 
-        if (strpos($temp, $book) !== false && strpos($temp, $res) === false) {
+        if (strpos($file, $book) !== false && strpos($file, $res) === false) {
             return true;
-        } else if (strpos($temp, $book) === false && strpos($temp, $res) !== false) {
+        } else if (strpos($file, $book) === false && strpos($file, $res) !== false) {
             return true;
+        } else if (strpos($file, $book) !== false && strpos($file, $res) !== false) {
+            return false;
         } return false;
 
     }
 
-    function checkFile($temp)
+    function checkFile()
     {
         $file1 = "Lorem Ipsum is simply dummy  text of the printing and typesetting industry. Lorem Ipsum hasbeen the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
         $file2 = "There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
@@ -38,4 +40,4 @@
         
     } 
 
-    checkFile($temp);
+    checkFile();
