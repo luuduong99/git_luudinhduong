8fb690f99ce91ecfa7a6d8fe07d32cf45607b3b9
LUU DUONG <luudinhduong2806@gmail.com>
2022-03-22 11:45:54 +0700

    fix php basic

--- exercise1.php
+++ exercise1.php
@@ -1,38 +1,34 @@
 <?php
     
-    function checkValidString($file)
+    function checkValidString($str)
     {
-        $book = "book";
-        $res = "restaurant";
+        $str1 = "book";
+        $str2 = "restaurant";
 
-        if (strpos($file, $book) !== false && strpos($file, $res) === false) {
-            return true;
-        } else if (strpos($file, $book) === false && strpos($file, $res) !== false) {
-            return true;
-        } else if (strpos($file, $book) !== false && strpos($file, $res) !== false) {
-            return false;
+        if ((strpos($str, $str1) !== false && strpos($str, $str2) === false) || (strpos($str, $str1) === false && strpos($str, $str2) !== false) ) { 
+            return true; 
         } return false;
 
     }
 
     function checkFile()
     {
-        $file1 = "Lorem Ipsum is simply dummy  text of the printing and typesetting industry. Lorem Ipsum hasbeen the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-        $file2 = "There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+        $str1 = "Lorem Ipsum is simply dummy  text of the printing and typesetting industry. Lorem Ipsum hasbeen the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        $str2 = "There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
 
-        $check1 = checkValidString($file1);
-        $check2 = checkValidString($file2);
+        $check1 = checkValidString($str1);
+        $check2 = checkValidString($str2);
         
         $count = 0;
         if ($check1) {
-            $count = substr_count($file1, ".");
+            $count = substr_count($str1, ".");
             echo "<h1>Chuỗi ở file1 là chuỗi hợp lệ và số câu là: $count</h1><br>";
         } else {
             echo "<h1>Chuỗi ở file1 là chuỗi không hợp lệ</h1><br>";
         }
         $count1 = 0;
         if ($check2) {
-            $count1 = substr_count($file2, ".");
+            $count1 = substr_count($str2, ".");
             echo "<h1>Chuỗi ở file2 là chuỗi hợp lệ và số câu là: $count1</h1><br>";
         } else {
             echo "<h1>Chuỗi ở file2 là chuỗi không hợp lệ</h1><br>";
