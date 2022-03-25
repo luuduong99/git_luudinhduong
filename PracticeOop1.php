37b49e8b713ec92ad77e28f82c9569cba034ad17
LUU DUONG <luudinhduong2806@gmail.com>
2022-03-25 16:42:11 +0700

    Fix php oop

--- PracticeOop1.php
+++ PracticeOop1.php
@@ -23,9 +23,9 @@
 
         function checkValidString($str, $text1, $text2) 
         {
-            if (((strstr($str, $text1) !== false) && (strstr($str, $text2) === false))
-            || ((strstr($str, $text1) === false) && (strstr($str, $text2) !== false))) {
-            return true;
+             if (((strstr($str, $text1) !== false) && (strstr($str, $text2) === false))
+             || ((strstr($str, $text1) === false) && (strstr($str, $text2) !== false))) {
+             return true;
         }
         return false;
         }
