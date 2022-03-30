76d07d20667c7da4a40a990a825f52ad3398430d
LUU DUONG <luudinhduong2806@gmail.com>
2022-03-30 16:49:50 +0700

    fix sql

--- Practice.sql
+++ Practice.sql
@@ -1,22 +1,23 @@
 -- Câu 1: Thông tin những bản ghi của categories và số lượng items của mỗi categories đó
     SELECT categories.*, count.num
     FROM categories
-    LEFT JOIN 
-    (SELECT category_id, COUNT(id) as num 
-    FROM items 
+    LEFT JOIN
+    (SELECT category_id, COUNT(id) as num
+    FROM items
     GROUP BY category_id) as count
     ON categories.id = count.category_id;
-
+    
 -- Câu 2:Thông tin những bản ghi của categories và tổng số amount của các items trong categories đó
     SELECT categories.*, total.amount
     FROM categories
-    LEFT JOIN 
-    (SELECT category_id, SUM(amount) as amount 
-    FROM items GROUP BY category_id) as total
+    LEFT JOIN
+    (SELECT category_id, SUM(amount) as amount
+    FROM items
+    GROUP BY category_id) as total
     ON categories.id = total.category_id;
 
 -- Câu 3:Thông tin những bản ghi của categories mà có ít nhất một items của nó có amount > 40
-    SELECT categories.*, items.id, items.amount 
+    SELECT categories.*, items.id, items.amount
     FROM categories
     LEFT JOIN items
     ON categories.id = items.category_id
