37b7dbb4c104f5cbd46d3347265189d225032ebe
LUU DUONG <luudinhduong2806@gmail.com>
2022-03-31 09:27:18 +0700

    fix php oop

--- PracticeOop1.php
+++ PracticeOop1.php
@@ -18,7 +18,6 @@
         public function readFile($file)
         {
             $data = file_get_contents($file);
-
             return $data;
         }
 
@@ -28,31 +27,39 @@
                 || ((strstr($str, $text1) === false) && (strstr($str, $text2) !== false))) {
                 return true;
             }
-
             return false;
         }
 
-        public function writeFile($text, $resultFile)
+        public function writeFile()
         {
-            $result = fopen($resultFile, "w") or die("Unable to open file!");
-            fwrite($result, $text);
-            fclose($result);
+            $resultFile = fopen("result_file.txt", "w") or die("Unable to open file!");
+            if ($this->check1 == true) {
+                fwrite($resultFile, "check1 là chuỗi hợp lệ\n");
+            } else {
+                fwrite($resultFile, "check1 là chuỗi không hợp lệ\n");
+            }
+
+            if ($this->check2 == true) {
+                fwrite($resultFile, "check2 là chuỗi hợp lệ\n");
+            } else {
+                fwrite($resultFile, "check2 là chuỗi không hợp lệ gao gồm " . substr_count(file_get_contents("file2.txt"),".") . " câu");
+            }
+            fclose($resultFile);
         }
     }
 
-    $text1 = "book";
-    $text2 = "restaurant";
-    $resultFile = "result_file.txt";
-    $object1 = new ExerciseString();
-    $data1 = $object1->readFile('file1.txt');
-    $data2 = $object1->readFile('file2.txt');
-    $count = substr_count($data2, ".");
-    $object1->check1 = $object1->checkValidString($data1, $text1, $text2);
-    var_dump($object1->getCheck1());    
-    $line1 = "Check1 là chuỗi " . ($object1->getCheck1() == true ? "Hợp lệ\n" : "Không hợp lệ\n");
-    echo "</br>";
-    $object1->check2 = $object1->checkValidString($data2, $text1, $text2);
-    var_dump($object1->getCheck2());    
-    $line2 = 'Check2 là chuỗi "' . ($object1->getCheck2() == true ? "hợp lệ" : "không hợp lệ") . '". Chuỗi có ' . $count . " câu.";
-    $text = $line1 . $line2;
-    $object1->writeFile($text, $resultFile);
+$keyWord1 = "book";
+$keyWord2 = "restaurant";
+
+$object1 = new ExerciseString();
+$data1 = $object1->readFile('file1.txt');
+$data2 = $object1->readFile('file2.txt');
+
+$object1->check1 = $object1->checkValidString($data1, $keyWord1, $keyWord2);
+var_dump($object1->getCheck1());    
+echo "</br>";
+
+$object1->check2 = $object1->checkValidString($data2, $keyWord1, $keyWord2);
+var_dump($object1->getCheck2());    
+
+$object1->writeFile();
